8b441ae74f2ee8b06a7fb32a0e55a2f643422edb
Alexander Nakahara <lexnakahara@gmail.com>
2021-10-18 20:16:48 -0300

    tratamento dos serviços de  classificação e barramento
    ajustes

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -8,22 +8,31 @@ app.use(bodyParser.json())
 const eventos = []
 
 app.post('/eventos', (req, res) => {
-    const evento = req.body
-    eventos.push(evento)
-    console.log(evento)
-    // envia o evento para o microsserviço de lembretes
-    axios.post('http://localhost:4000/eventos', evento)
-    // envia o evento para o microsserviço de observações
-    axios.post('http://localhost:5000/eventos', evento)
-    //envia o evento para o microsserviço de consulta
-    axios.post('http://localhost:6000/eventos', evento)
-    // envia o evento para o microsserviço de classificação
-    axios.post('http://localhost:7000/eventos', evento)
-    res.status(204).end()
+    
+    try {
+        const evento = req.body
+        eventos.push(evento)
+        console.log(evento)
+        // envia o evento para o microsserviço de lembretes
+        axios.post('http://localhost:4000/eventos', evento)
+        // envia o evento para o microsserviço de observações
+        axios.post('http://localhost:5000/eventos', evento)
+        //envia o evento para o microsserviço de consulta
+        axios.post('http://localhost:6000/eventos', evento)
+        // envia o evento para o microsserviço de classificação
+        axios.post('http://localhost:7000/eventos', evento)
+        res.status(204).end()
+        
+    } catch (error) {}
+
+   
 })
 
 app.get('/eventos', (req, res) => {
-    res.send(eventos)
+    try {
+        res.send(eventos)
+        
+    } catch (error) {}
 })
 
 app.listen(10000, () => console.log("Barramento de eventos. Porta 10000."))
--- classificacao/index.js
+++ classificacao/index.js
@@ -25,4 +25,14 @@ app.post('/eventos', (req, res) => {
     res.status(200).send({msg: 'ok'})
 })
 
-app.listen(7000, () => console.log ("Classificação. Porta 7000."))
+app.listen(7000, async() => {
+    console.log ("Classificação. Porta 7000.")
+    const resp = await axios.get('http://localhost:10000/eventos')
+
+    resp.data.forEach((valor, indice, colecao) => {
+        try{
+            funcoes[valor.tipo](valor.dados)
+        }
+        catch (e){}
+    })
+})
